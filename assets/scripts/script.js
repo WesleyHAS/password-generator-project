7c1a8549c1eec9aca609042181bbbec2c73c5dd8
Wes <wesley.has@outlook.com>
2023-07-25 14:19:07 -0600

    added prompt for lenght

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,6 +6,16 @@ function generatePassword() {
   var uppercaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var symbolChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?'];
+
+  //prompt to ask for password lenght
+  var length = prompt("Enter the password length (between 8 and 32 characters):");
+
+  // Validate the password length
+  if (length < 8 || length > 32) {
+    alert("Invalid password length. Please enter a number between 8 and 32.");
+    return;
+  }
+
 }
 
 // Get references to the #generate element
