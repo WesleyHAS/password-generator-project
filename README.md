255732d655ef3748f6c750acbaf13a3e954d241e
Wes <wesley.has@outlook.com>
2023-07-25 13:38:33 -0600

    added all character available

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# password-generator-project
+# Module 3 Project
+
+## Description
+
+This a password generator project as a part of week 3 assignments for the UofT Flex Bootcamp.
+
+## Installation
+
+N/A
+
+## Usage
+
+Please visit following link to see final results 
+
+
+
+## Preview of Page
+
+![Screentshot 1](./assets/images/page-preview-1.png)
+
+![Screentshot 2](./assets/images/page-preview-2.png)
+
+## Credits
+
+Credits to Xandromus who is the original creator of this code.
+
+https://github.com/coding-boot-camp/friendly-parakeet
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,12 @@
 // Assignment code here
 
+function generatePassword() {
+  //all characters available
+  var lowercaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var uppercaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var symbolChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?'];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
